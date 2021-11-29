b8584c08db8a32b8bc2899cf91f92e9b06474f67
Brawan89 <Rawan.Badr.elq.js@tuwaiq.edu.sa>
2021-11-29 22:38:27 +0300

    hash

--- Routers/Controllers/user.js
+++ Routers/Controllers/user.js
@@ -1,11 +1,22 @@
 const userModel = require("./../../db/models/user");
 
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const SALT = Number(process.env.SALT);
+
+const SECRET = process.env.SECRETKAY;
+
 //create users
-const register = (req, res) => {
+const register = async (req, res) => {
   const { email, password, role } = req.body;
+
+  const saveEmail = email.toLowerCase();
+  const hashedPass = await bcrypt.hash(password, SALT);
+
   const newUser = new userModel({
-    email,
-    password,
+    email: saveEmail,
+    password: hashedPass,
     role,
   });
   newUser
@@ -23,13 +34,22 @@ const register = (req, res) => {
 //login
 const login = (req, res) => {
   const { email, password } = req.body;
+  const saveEmail = email.toLowerCase();
   userModel
-    .findOne({ email })
-    .then((result) => {
+    .findOne({ email: saveEmail })
+    .then( async (result) => {
       if (result) {
         if (result.email == email) {
-          if (result.password == password) {
-            res.status(200).json(result);
+          const hashedPass = await bcrypt.compare(password, result.password);
+          if (hashedPass) {
+            const payload ={
+              role: result.role,
+            };
+            const options = {
+              expiresIn: "60m",
+            }
+            const token = await jwt.sign(payload, SECRET, options);
+            res.status(200).json({result, token});
           } else {
             res.status(400).json("invalid email or passowrd");
           }
